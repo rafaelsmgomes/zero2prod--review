3327cb4930e1f637f8b5506dd3dd0dbb3c5a730b
rafaelsmgomes <rafael.smgomes@gmail.com>
2023-09-24 09:21:23 -0600

    5.3.8 - Optimizing our docker image
    Everything is working but our image is huge right now

--- Dockerfile
+++ Dockerfile
@@ -7,5 +7,6 @@ run apt update && apt install lld clang -y
 copy . .
 env SQLX_OFFLINE true
 run cargo build --release
+env APP_ENVIRONMENT production
 
 entrypoint ["./target/release/zero2prod-review"]
--- src/configuration.rs
+++ src/configuration.rs
@@ -23,10 +23,48 @@ pub struct DatabaseSettings {
 
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
     let mut settings = config::Config::default();
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
 
-    settings.merge(config::File::with_name("configuration"))?;
+    settings.merge(config::File::from(configuration_directory.join("base")).required(true))?;
 
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+    // Layer on the environment-specific values.
+    settings.merge(
+        config::File::from(configuration_directory.join(environment.as_str())).required(true),
+    )?;
     settings.try_into()
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+    fn try_from(value: String) -> Result<Self, Self::Error> {
+        match value.to_lowercase().as_str() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either 'local' or 'production'.",
+                other
+            )),
+        }
+    }
 }
 
 impl DatabaseSettings {
