f32bac3843b060779207b2e6b671c999bd68ca04
rafaelsmgomes <rafael.smgomes@gmail.com>
2023-09-30 02:28:51 -0600

    Commit detour while I track problems with the tests

--- src/email_client.rs
+++ src/email_client.rs
@@ -76,28 +76,7 @@ mod tests {
     use fake::{Fake, Faker};
     use secrecy::Secret;
     use wiremock::matchers::{any, header, header_exists, method, path};
-    use wiremock::Request;
-    use wiremock::{Mock, MockServer, ResponseTemplate};
-
-    fn subject() -> String {
-        Sentence(1..2).fake()
-    }
-    fn content() -> String {
-        Paragraph(1..10).fake()
-    }
-
-    fn email() -> SubscriberEmail {
-        SubscriberEmail::parse(SafeEmail().fake()).unwrap()
-    }
-
-    fn email_client(base_url: String) -> EmailClient {
-        EmailClient::new(
-            base_url,
-            email(),
-            Secret::new(Faker.fake()),
-            std::time::Duration::from_millis(200),
-        )
-    }
+    use wiremock::{Mock, MockServer, Request, ResponseTemplate};
 
     struct SendEmailBodyMatcher;
 
@@ -116,8 +95,34 @@ mod tests {
         }
     }
 
+    /// Generate a random email subject
+    fn subject() -> String {
+        Sentence(1..2).fake()
+    }
+
+    /// Generate a random email content
+    fn content() -> String {
+        Paragraph(1..10).fake()
+    }
+
+    /// Generate a random subscriber email
+    fn email() -> SubscriberEmail {
+        SubscriberEmail::parse(SafeEmail().fake()).unwrap()
+    }
+
+    /// Get a test instance of `EmailClient`.
+    fn email_client(base_url: String) -> EmailClient {
+        EmailClient::new(
+            base_url,
+            email(),
+            Secret::new(Faker.fake()),
+            std::time::Duration::from_millis(200),
+        )
+    }
+
     #[tokio::test]
     async fn send_email_sends_the_expected_request() {
+        // Arrange
         let mock_server = MockServer::start().await;
         let email_client = email_client(mock_server.uri());
 
@@ -131,13 +136,17 @@ mod tests {
             .mount(&mock_server)
             .await;
 
+        // Act
         let _ = email_client
             .send_email(email(), &subject(), &content(), &content())
             .await;
+
+        // Assert
     }
 
     #[tokio::test]
     async fn send_email_succeeds_if_the_server_returns_200() {
+        // Arrange
         let mock_server = MockServer::start().await;
         let email_client = email_client(mock_server.uri());
 
@@ -147,48 +156,56 @@ mod tests {
             .mount(&mock_server)
             .await;
 
+        // Act
         let outcome = email_client
             .send_email(email(), &subject(), &content(), &content())
             .await;
 
+        // Assert
         assert_ok!(outcome);
     }
 
     #[tokio::test]
     async fn send_email_fails_if_the_server_returns_500() {
+        // Arrange
         let mock_server = MockServer::start().await;
         let email_client = email_client(mock_server.uri());
 
         Mock::given(any())
+            // Not a 200 anymore!
             .respond_with(ResponseTemplate::new(500))
             .expect(1)
             .mount(&mock_server)
             .await;
 
+        // Act
         let outcome = email_client
             .send_email(email(), &subject(), &content(), &content())
             .await;
 
+        // Assert
         assert_err!(outcome);
     }
 
     #[tokio::test]
-    async fn send_email_times_out_uf_the_server_takes_too_long() {
+    async fn send_email_times_out_if_the_server_takes_too_long() {
+        // Arrange
         let mock_server = MockServer::start().await;
         let email_client = email_client(mock_server.uri());
 
         let response = ResponseTemplate::new(200).set_delay(std::time::Duration::from_secs(180));
-
         Mock::given(any())
             .respond_with(response)
             .expect(1)
             .mount(&mock_server)
             .await;
 
+        // Act
         let outcome = email_client
             .send_email(email(), &subject(), &content(), &content())
             .await;
 
+        // Assert
         assert_err!(outcome);
     }
 }
